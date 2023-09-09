bb1d66083e99e436ac6e0ad79d16f26b8f9c3ad9
Varchaswa <varchaswacodes@gmail.com>
2023-09-09 20:12:48 +0530

    First commit

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-1sds06r22gh55xct.us.auth0.com"
+     clientId="CjPYPoFadc5a2cowW3QPvU3rM2K89HsZ"
+     authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
